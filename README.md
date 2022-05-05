5eb06b26a6b868e178ffd77893730aa3bf759319
talamcol <talamcol>
2022-05-05 16:04:18 +0200

    feat: initial implementation

--- README.md
+++ README.md
@@ -1,26 +1,21 @@
-To use this template create a new repository and select `example-pkg` from the template dropdown. Make sure to name your repository following `<name>-pkg` convention. Make sure to apply branch protection rules to `main` branch and enable `dependabot` for security updates.
-If you're interested in automated dependency updates make sure that `renovate` has access to the new repository.
-
-# Things to do change when you use this repository as a template:
-- [ ] replace `example-pkg` with the name of your package in this `README`
-- [ ] update the `@smg-automotive/example` with the name of your package in `package.json`
-- [ ] update `repository` and `homepage` sections in `package.json` to point to your GitHub repository
-- [ ] ensure that Frontend team has the admin access to the repository
-- [ ] ensure that the Bots team has write access to the repository (this is needed to release the package)
 - [ ] provide usage examples in the `README.md`
 - [ ] enable the project on circleci.com to build and test your package
 - [ ] change the circleci status badge in this `README` to the new project
 - [ ] develop an awesome package
 - [ ] live long and prosper
-# example-pkg
+
+# api-client-pkg
 
 [![CircleCI](https://circleci.com/gh/smg-automotive/example-pkg/tree/main.svg?style=svg&circle-token=c183f151fea3c74453cf8dd962d31e115906a300)](https://circleci.com/gh/smg-automotive/example-pkg/tree/main)
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
 
 ## Usage
 ```
-npm install @smg-automotive/example
+npm install @smg-automotive/api-client-pkg
 ```
+
+### Configuration
+The available instance methods are listed below. The specified config will be merged with the instance config.
 
 ## Development
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,91 @@
-export default () => {
-  // eslint-disable-next-line no-console
-  console.log('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+export interface ApiClientConfiguration {
+  baseUrl: string
+  headers: Record<string, string>
 }
+
+export interface RequestOptions extends ApiClientConfiguration {
+  params?: Record<string, string | number>
+}
+
+class ApiClient {
+  // eslint-disable-next-line no-use-before-define
+  private static instance: ApiClient
+
+  private configuration: ApiClientConfiguration
+
+  private constructor() {
+    this.configuration = {
+      baseUrl: '',
+      headers: { 'Content-Type': 'application/json' },
+    }
+  }
+
+  public static getInstance(): ApiClient {
+    if (!ApiClient.instance) {
+      ApiClient.instance = new ApiClient()
+    }
+    return ApiClient.instance
+  }
+
+  private getPath(path: string, options?: RequestOptions) {
+    const baseUrl = options?.baseUrl || this.configuration.baseUrl
+    if (!baseUrl) {
+      throw new Error(
+        'ApiClient is not configured. Please run ApiClient.configure()',
+      )
+    }
+
+    // TODO: magic with replacement
+
+    return `${baseUrl}${path}`
+  }
+
+  configure = (configuration: ApiClientConfiguration) => {
+    this.configuration = configuration
+  }
+
+  get = <ResponseType>(
+    path: string,
+    options?: RequestOptions,
+  ): Promise<ResponseType> => {
+    return fetch(this.getPath(path, options)).then((res) => {
+      return res.json()
+    })
+  }
+
+  post = <ResponseType, RequestType>(
+    path: string,
+    body: RequestType,
+    options?: RequestOptions,
+  ): Promise<ResponseType> => {
+    return fetch(this.getPath(path, options), {
+      method: 'POST',
+      body: JSON.stringify(body),
+    }).then((res) => {
+      return res.json()
+    })
+  }
+
+  put = <ResponseType, RequestType>(
+    path: string,
+    body: RequestType,
+    options?: RequestOptions,
+  ): Promise<ResponseType> => {
+    return fetch(this.getPath(path, options), {
+      method: 'PUT',
+      body: JSON.stringify(body),
+    }).then((res) => {
+      return res.json()
+    })
+  }
+
+  delete = (path: string, options?: RequestOptions): Promise<void> => {
+    return fetch(this.getPath(path, options), {
+      method: 'DELETE',
+    }).then((res) => {
+      return res.json()
+    })
+  }
+}
+
+export default ApiClient.getInstance()
