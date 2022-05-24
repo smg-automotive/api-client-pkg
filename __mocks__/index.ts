b6c70480e4f5e78e1f5f5e74853824cab05ea7cf
talamcol <talamcol>
2022-05-24 16:23:16 +0200

    chore: do not use return

--- __mocks__/index.ts
+++ __mocks__/index.ts
@@ -1,18 +1,16 @@
 export const ApiClient = () => ({
-  path: () => {
-    return {
-      get: jest
-        .fn()
-        .mockReturnValue(Promise.resolve({ status: 200, body: {}, ok: true })),
-      post: jest
-        .fn()
-        .mockReturnValue(Promise.resolve({ status: 201, body: {}, ok: true })),
-      put: jest
-        .fn()
-        .mockReturnValue(Promise.resolve({ status: 204, body: {}, ok: true })),
-      delete: jest
-        .fn()
-        .mockReturnValue(Promise.resolve({ status: 204, body: {}, ok: true })),
-    };
-  },
+  path: () => ({
+    get: jest
+      .fn()
+      .mockReturnValue(Promise.resolve({ status: 200, body: {}, ok: true })),
+    post: jest
+      .fn()
+      .mockReturnValue(Promise.resolve({ status: 201, body: {}, ok: true })),
+    put: jest
+      .fn()
+      .mockReturnValue(Promise.resolve({ status: 204, body: {}, ok: true })),
+    delete: jest
+      .fn()
+      .mockReturnValue(Promise.resolve({ status: 204, body: {}, ok: true })),
+  }),
 });
