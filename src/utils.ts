8a397faea1fa256d124451713186e1e523a355dd
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-11-08 15:04:13 +0100

    fix: turns out simple impl was better

--- src/utils.ts
+++ src/utils.ts
@@ -1,28 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type TupleKeys<T extends ReadonlyArray<any>> = Exclude<keyof T, keyof any[]>;
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type IsTuple<T extends ReadonlyArray<any>> = number extends T['length']
-  ? false
-  : true;
-
-type ArrayKey = number;
-
-type Primitive = null | undefined | string | number | boolean | symbol | bigint;
-type BrowserNativeObject = Date | FileList | File;
-
-type PathImpl<K extends string | number, V> = V extends
-  | Primitive
-  | BrowserNativeObject
-  ? `${K}`
-  : `${K}` | `${K}.${Path<V>}`;
-
-export type Path<T> = T extends ReadonlyArray<infer V>
-  ? IsTuple<T> extends true
-    ? {
-        [K in TupleKeys<T>]-?: PathImpl<K & string, T[K]>;
-      }[TupleKeys<T>]
-    : PathImpl<ArrayKey, V>
-  : {
-      [K in keyof T]-?: PathImpl<K & string, T[K]>;
-    }[keyof T];
+export type Path<ObjectType extends object> = {
+  [Key in keyof ObjectType & (string | number)]: ObjectType[Key] extends object
+    ? `${Key}` | `${Key}.${Path<ObjectType[Key]>}`
+    : `${Key}`;
+}[keyof ObjectType & (string | number)];
