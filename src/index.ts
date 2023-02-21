442d6eab0ced7b786a526bc407a8cadca29bdaac
talamcol <colin@talamona.ch>
2023-02-21 13:14:24 +0100

    fix: lint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { SortedQuery, SortParams, SortQuery, SortOrderParams } from './sort';
+import { SortedQuery, SortOrderParams, SortParams, SortQuery } from './sort';
 import { Sanitizers } from './sanitizers';
 import { ErrorResponse, ResponseType, SuccessResponse } from './responseType';
 import { replaceParameters } from './pathParameters';
@@ -95,7 +95,7 @@ export {
   PaginatedResponse,
   PaginationQuery,
   SortQuery,
+  SortOrderParams,
   SortParams,
   SortedQuery,
-  SortOrderParams
 };
