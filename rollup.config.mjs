5f683cd1adb8a9f3cfabdc517cac2e171470be48
Krzysztof Sakwerda <ksakwerda@gmail.com>
2024-10-09 11:33:53 +0200

    chore: adjust rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,25 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import dts from 'rollup-plugin-dts';
+import { dirname } from 'path';
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 import packageJson from './package.json' assert { type: 'json' };
 
+const jsPlugins = [
+  peerDepsExternal(),
+  resolve({ moduleDirectories: ['.', 'node_modules'] }),
+];
+
 export default [
   {
     input: '__mocks__/index.ts',
     output: [{ file: 'dist/__mocks__/index.js', format: 'cjs' }],
-    plugins: [typescript({ tsconfig: '__mocks__/tsconfig.json' })],
+    plugins: [typescript({
+      tsconfig: '__mocks__/tsconfig.json',
+      outDir: 'dist/__mocks__',
+    })],
   },
   {
     input: '__mocks__/index.ts',
@@ -25,6 +34,20 @@ export default [
         format: 'cjs',
         sourcemap: true,
       },
+    ],
+    plugins: [
+      ...jsPlugins,
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.build.json',
+        outDir: dirname(packageJson.main),
+      }),
+    ],
+    external: ['react', 'react-dom'],
+  },
+  {
+    input: 'src/index.ts',
+    output: [
       {
         file: packageJson.module,
         format: 'esm',
@@ -32,16 +55,17 @@ export default [
       },
     ],
     plugins: [
-      peerDepsExternal(),
-      resolve({ moduleDirectories: ['.', 'node_modules'] }),
-      commonjs(),
-      typescript({ tsconfig: './tsconfig.build.json' }),
+      ...jsPlugins,
+      typescript({
+        tsconfig: './tsconfig.build.json',
+        outDir: dirname(packageJson.module),
+      }),
     ],
     external: ['react', 'react-dom'],
   },
   {
     input: 'src/index.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    output: [{ file: packageJson.types, format: 'esm' }],
     plugins: [dts()],
   },
 ];
